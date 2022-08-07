baa072ddacbf67b004ceb6fa704d01945b4a57ad
Phil564 <93886661+Phil564@users.noreply.github.com>
2022-08-07 10:46:21 -0400

    revised yesterday's release
    i fucked up the localStorage (cookies) script so i changed it to the correct version

--- localStorage.js
+++ localStorage.js
@@ -1,62 +1,47 @@
 //Phil's note: credits to this repo https://github.com/gr2m/localstorage-tutorial for literally teaching me how to do this black magic.
+if (typeof(Storage) !== "undefined") {
 var $form = document.querySelector('#add-form')
-var $table = document.querySelector('#list-table')
+var $removeform = document.querySelector('#remove-form')
 
 var playtesterids = JSON.parse(localStorage.getItem('playtesterids') || '[]')
 
 playtesterids.forEach(function (playtesterid) {
-  var $row = document.createElement('tr')
-  $row.dataset.id = playtesterid.id
-  $row.innerHTML = `
-    <td>
-      <div class="uid">${playtesterid.name}</div>
-    </td>
-    <td class="actions">
-      <a href="#" data-action="delete">delete</a>
-    </td>
-  `
-  $table.appendChild($row)
+	var $login2 = document.getElementById("login2")
+	$login2.dataset.id = playtesterid.id
+	$login2.innerHTML = `
+		Login Successful<br>Your playtester ID (do not share): <div class="uid">${playtesterid.name}</div>
+	`
 })
 
 $form.addEventListener('submit', function (event) {
-  event.preventDefault()
+	event.preventDefault()
+	
+	var name = document.querySelector('#name').value
+	var id = Math.random().toString(36).substr(2, 7)
+	var $login2 = document.getElementById("login2")
+	$login2.dataset.id = id
+	$login2.innerHTML = `
+		Login Successful<br>Your playtester ID (do not share): <div class="uid">${name}</div>
+	`
 
-  var name = document.querySelector('#name').value
-  var id = Math.random().toString(36).substr(2, 7)
-  var $row = document.createElement('tr')
-  $row.dataset.id = id
-  $row.innerHTML = `
-    <td>
-      Login Successful<br>Your playtester ID (do not share): <div class="uid">${name}</div>
-    </td>
-    <td class="actions">
-      <a href="#" data-action="delete">delete</a>
-    </td>
-  `
-  $table.appendChild($row)
+	$form.reset()
 
-  $form.reset()
-
-  playtesterids.push({
-    id: id,
-    name: name
-  })
-  localStorage.setItem('playtesterids', JSON.stringify(playtesterids))
+	playtesterids.push({
+		id: id,
+		name: name
+	})
+	localStorage.setItem('playtesterids', JSON.stringify(playtesterids))
 })
 
-$table.addEventListener('click', function (event) {
-  event.preventDefault()
+$removeform.addEventListener('click', function (event) {
+	event.preventDefault()
 
-  var $button = event.target
-  var $row = $button.closest('tr')
-  var id = $row.dataset.id
-  var action = $button.dataset.action
-
-  if (action === 'delete') {
-    playtesterids = playtesterids.filter(function (playtesterid) {
-      return playtesterid.id !== id
-    })
-    localStorage.setItem('playtesterids', JSON.stringify(playtesterids))
-    $row.remove()
-  }
+	localStorage.clear()
+	location.reload()
 })
+}
+else {
+document.getElementById("login2").innerHTML = `
+		Unable to connect to main servers.<br>Check if JavaScript support is enabled in your browser.<br>Try refreshing the page.
+	`
+}
